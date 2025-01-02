72f83fee782622ecc24ef6db6f9cc13c4249dc2c
David-Currey <davecurrey76@gmail.com>
2025-01-02 14:53:03 -0500

    complete

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function createElement (type, props, ...children) {
-  const $el = document.createElement(type)
-  Object.keys(props).forEach(prop => $el[prop] = props[prop])
-  $el.append(...children)
-  return $el
+const lib = document.querySelector('#library')
+
+createBooks(books)
+
+function createElement(type, props, ...children) {
+	const el = document.createElement(type)
+	Object.keys(props).forEach((prop) => (el[prop] = props[prop]))
+	el.append(...children)
+	return el
+}
+
+function createBooks(bookArray) {
+	bookArray.forEach((book) => {
+		const bookItem = createElement(
+			'div',
+			{ className: 'book' },
+			createElement('img', {
+				src: book.image,
+				className: 'img-fluid',
+				alt: `Cover of ${book.title}`,
+			})
+		)
+		lib.appendChild(bookItem)
+	})
 }
